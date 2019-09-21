e023636939a0db7bfea09e98699bef367f1e664f
kingoftheconnors <Connormcp777@gmail.com>
2019-09-21 17:47:06 -0400

    Added triggers unactivating and gold of all players being saved

--- assets_src/lua/saveState.lua
+++ assets_src/lua/saveState.lua
@@ -1,4 +1,5 @@
 local Wargroove = require "wargroove/wargroove"
+local Events = require "wargroove/events"
 
 -- Savestate is a global variable. This builds saveState if it doesn't exist, but lets it be otherwise
 
@@ -48,18 +49,24 @@ if(not saveState) then
 
     function saveState:save(playerId)
         local newState = {}
-        -- Save your player's gold value
-        newState.gold = Wargroove.getMoney(playerId)
+        -- Save your players' gold values
+        newState.gold = {}
+        for id = 0, Wargroove.getNumPlayers(false)-1 do
+            newState.gold[id] = Wargroove.getMoney(id)
+        end
 
         -- Save states of all units
         saveUnits = {}
         for _, unit in ipairs(Wargroove.getUnitsAtLocation()) do
             -- Copy unit into saveUnits
             local copiedTable = self:copyTable(unit)
-            print(copiedTable.id)
-            table.insert(saveUnits, copiedTable)
+            --print(copiedTable.id, saveUnits[unit.id]) -- here for testing purposes
+            saveUnits[unit.id] = copiedTable
         end
         newState.units = saveUnits
+
+        -- Save map counters, flags, campaign flags, party and fired triggers
+        newState.matchState = Events.getMatchState()
 
         -- Extensible section if this module is used in other mods
         self:modSpecificSave(newState)
@@ -74,17 +81,51 @@ if(not saveState) then
 
     function saveState:load(playerId)
         -- Load gold
-        Wargroove.setMoney(playerId, saveState.loadedState.gold)
+        for id = 0, Wargroove.getNumPlayers(false)-1 do
+            Wargroove.setMoney(id, saveState.loadedState.gold[id])
+        end
+
+        -- Load map counters
+        local matchState = Events.getMatchState()
+        matchState = saveState.loadedState.matchState
+
+        -- Reset Match State so triggers will now recognize changed counters and flags
+        Events.startSession(matchState)
 
         -- Extensible section if this module is used in other mods
         self:modSpecificLoad(playerId)
     end
 
+    function saveState:respawnedUnitCorrections(unit, unitInfo)
+        print("Setting " .. unit.unitClassId .. "'s health to", unitInfo.health)
+        unit:setHealth(unitInfo.health, unit.id)
+    end
+
     function saveState:loadOnPost(playerId)
-        for _, unit in ipairs(saveState.loadedState.units) do
-            local u = Wargroove.getUnitById(unit.id)
-            self:overrideTable(u, unit)
-            Wargroove.updateUnit(u)
+        for _, unit in ipairs(Wargroove.getUnitsAtLocation()) do
+            if(saveState.loadedState.units[unit.id] ~= nil) then
+                -- We are copying over instead of changing the reference because we don't save unitClass
+                self:overrideTable(unit, saveState.loadedState.units[unit.id])
+                saveState.loadedState.units[unit.id] = nil
+                Wargroove.updateUnit(unit)
+            else
+                -- This unit did not exist before. Kill it
+                unit:setHealth(0, unit.id)
+                Wargroove.updateUnit(unit)
+            end
+        end
+
+        -- Bring units BACK
+        -- SELF: Should this keep dying units alive and bring them back if necessary or just reset their values?
+        -- Probably reset values for generality and modularity's sake
+        for _, unit in pairs(saveState.loadedState.units) do
+            print("Unit ", unit, unit.unitClassId)
+            Wargroove.spawnUnit(unit.playerId, unit.pos, unit.unitClassId, unit.hadTurn)
+            
+            Wargroove.waitFrame()
+
+            print("Unit", unit.unitClassId, Wargroove.getUnitAt(unit.pos))
+            self:respawnedUnitCorrections(Wargroove.getUnitAt(unit.pos), unit)
         end
 
         -- Extensible section if this module is used in other mods
@@ -99,7 +140,7 @@ end
 return saveState
 
 -- Todo:
--- When loading, check if there are any NEW units, and destroy them
--- Save and load gold of other players
--- Save and load map counters and flags
--- Save and load trigger flags
+-- Resurrecting dead units
+-- Deep copy of unit tables and changing references rather than copying back and over(?)
+-- Test how much memory having, say, 50 states uses on the system
+-- If the memory is too high, change it so each state only tracks changes to the game map
